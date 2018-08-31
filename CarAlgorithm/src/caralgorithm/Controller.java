98bbfb2f1b1e7591477787404b7ec5568f94b60c
gerome <gerome.meyer@icloud.com>
2018-08-31 14:10:41 +0200

    Phase 1

--- CarAlgorithm/src/caralgorithm/Controller.java
+++ CarAlgorithm/src/caralgorithm/Controller.java
@@ -1,5 +1,16 @@
 package caralgorithm;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
-    private int bla;
+
+    @FXML
+    private Label fieldOne;
+
+    @FXML
+    public void handleButtonStart(ActionEvent event) {
+        fieldOne.setId("red");
+    }
 }
