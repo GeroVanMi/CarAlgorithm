96c5a84c30765395a904a075104ae2d2e9bcd0d8
GeroVanMi <39983753+GeroVanMi@users.noreply.github.com>
2018-09-07 12:27:23 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,45 @@
+package caralgorithm;
+
+public class Car {
+    private int velocity, maxVelocity, pos;
+    private double flea;
+
+    public Car(int velocity, int maxVelocity, double flea, int pos) {
+        this.velocity = velocity;
+        this.maxVelocity = maxVelocity;
+        this.flea = flea;
+        this.pos = pos;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public double getFlea() {
+        return flea;
+    }
+
+    public int getMaxVelocity() {
+        return maxVelocity;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public void incVelocity() {
+        this.velocity++;
+    }
+
+    public void decVelocity() {
+        this.velocity--;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,24 @@
+package caralgorithm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gameField.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Phantomstau");
+        primaryStage.setScene(new Scene(root, 1280, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
