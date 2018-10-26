851d6807ce4a2ddc61780a141ef3b40c049bf0df
Natalie Breu <nabreu@bluewin.ch>
2018-10-26 13:20:50 +0200

    Merge branch 'master' of E:\_FILES\2_KBW\AngewandteMathematik\CarAlgorithm with conflicts.

--- CarAlgorithm/src/ch/kbw/caralgorithm/Main.java
+++ CarAlgorithm/src/ch/kbw/caralgorithm/Main.java
@@ -1,4 +1,4 @@
-package ch.kbw.caralgorithm;
+package caralgorithm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,19 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/view.fxml"));
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("gameField.fxml"));
         primaryStage.setResizable(false);
         primaryStage.setTitle("Phantomstau");
-        primaryStage.setScene(new Scene(root, 1280, 600));
+        primaryStage.setScene(new Scene(root, 1300, 950));
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
+
+// In case of push error 403: add https://GeroVanMi@github.com/GeroVanMi/CarAlgorithm.git
