bb328a80e4706701f42fb90e65bb11cb37e18541
gerome <gerome.meyer@icloud.com>
2018-11-09 13:56:51 +0100

    Navigation updated;

--- CarAlgorithm/src/ch/kbw/caralgorithm/viewControllers/NavigationController.java
+++ CarAlgorithm/src/ch/kbw/caralgorithm/viewControllers/NavigationController.java
@@ -5,31 +5,48 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 public class NavigationController {
 
     @FXML
     private AnchorPane contentPane;
+    @FXML
+    private Label title;
+
+    public void setUp() {
+        loadContent("../../resources/fxml/home.fxml");
+        title.setText("Nagel-Schreckenberg-Modell Home");
+    }
 
     @FXML
     public void handleButtonHistory(ActionEvent ae) {
-         try {
-             Pane pane = FXMLLoader.load(getClass().getResource("../../resources/fxml/history.fxml"));
-             contentPane.getChildren().add(pane);
-             System.out.println("It worked?");
-         } catch (Exception ex) {
-            // TODO: NULL
-             ex.printStackTrace();
-         }
+         loadContent("../../resources/fxml/history.fxml");
+         title.setText("Nagel-Schreckenberg-Modell History");
     }
 
     @FXML
     public void handleButtonHome(ActionEvent e) {
-
+        loadContent("../../resources/fxml/home.fxml");
+        title.setText("Nagel-Schreckenberg-Modell Home");
     }
 
     @FXML
     public void handleButtonStatistics(ActionEvent e) {
+        loadContent("../../resources/fxml/statistics.fxml");
+        title.setText("Nagel-Schreckenberg-Modell Statistics");
+    }
 
+    public void loadContent(String path) {
+        try {
+            for(int i = 0; i < contentPane.getChildren().size(); i++) {
+                contentPane.getChildren().remove(0);
+            }
+            Pane pane = FXMLLoader.load(getClass().getResource(path));
+            contentPane.getChildren().add(pane);
+        } catch (Exception ex) {
+            // TODO: NULL
+            ex.printStackTrace();
+        }
     }
 }
