af34cc8b8e5c5cd6d9cce651c551384a3d7f1a45
Natalie Breu <nabreu@bluewin.ch>
2018-12-14 13:36:45 +0100

    destroyed lanes

--- src/main/java/viewControllers/HistoryController.java
+++ src/main/java/viewControllers/HistoryController.java
@@ -1,13 +1,36 @@
 package viewControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import models.Algorithm;
+import models.Field;
+import models.Lane;
+
 /**
  *
  */
-public class HistoryController extends ViewController
-{
+public class HistoryController extends ViewController {
+    @FXML
+    private VBox playgrounds;
+    private Algorithm algorithm;
+    private Lane lane;
+
+
+    //like constructor
     @Override
     public void setup(NavigationController navigationController) {
+        this.algorithm = navigationController.getAlgorithm();
+        playgrounds.getChildren().clear();
 
+        for (int i = 0; i < 20; i++) {
+            HBox hBox = new HBox();
+
+            for (Field f : algorithm.getLane().getFields()) {
+                hBox.getChildren().add(f.getLabel());
+            }
+            playgrounds.getChildren().add(hBox);
+        }
     }
 
     @Override
