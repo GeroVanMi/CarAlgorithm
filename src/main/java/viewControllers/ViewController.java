98b43077578cc843e76ebbac878026f216613bd7
gerome <gerome.meyer@icloud.com>
2018-12-31 17:18:40 +0100

    Finalizing

--- src/main/java/viewControllers/ViewController.java
+++ src/main/java/viewControllers/ViewController.java
@@ -1,6 +1,6 @@
 package viewControllers;
 
-public abstract interface ViewController {
+public interface ViewController {
     /**
      * Wird beim Erstellen eines Controllers aufgerufen und agiert als eine Art Konstruktor.
      * @param navigationController Der "NavigationsController" muss bei jedem Viewcontroller gesetzt werden und agiert als Eltern-Node.
